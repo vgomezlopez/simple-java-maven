0a5e9fe262c2eeedbeb47627cb55990436bd1e02
Victor Gomez <vicccc@df.fr>
2018-05-07 15:44:13 +0200

    First commit

---
+++ src/main/java/com/formation/app/App.java
@@ -0,0 +1,15 @@
+package com.formation.app;
+
+public class App {
+    private final String message = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+}
